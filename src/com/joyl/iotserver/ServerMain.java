05cb5b9584a3db5069c260d3ae7735b78cb1c77b
Kate Lim <meekyung.lim@gmail.com>
2013-06-20 08:59:39 -0400

    Add Controller Requet Handler
    Add Controller Requet Handler
    (Just Outline)

--- src/com/joyl/iotserver/ServerMain.java
+++ src/com/joyl/iotserver/ServerMain.java
@@ -5,6 +5,9 @@ import org.vertx.java.core.Vertx;
 import org.vertx.java.core.VertxFactory;
 import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.net.NetSocket;
 
 public class ServerMain {
@@ -14,28 +17,14 @@ public class ServerMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		RouteMatcher rm = new RouteMatcher();
+		initializeRouteMatcher(rm);
+
 		System.out.println("started");
 		Vertx vertx = VertxFactory.newVertx("localhost");
-		vertx.createHttpServer()
-				.requestHandler(new Handler<HttpServerRequest>() {
-					public void handle(HttpServerRequest req) {
-						System.out.println("Got request: " + req.uri());
-						System.out.println("Headers are: ");
-						for (String key : req.headers().names()) {
-							System.out.println(key + ":"
-									+ req.headers().get(key));
-						}
-						req.response()
-								.headers()
-								.add("Content-Type", "json; charset=UTF-8");
-						req.response()
-								.end("{ \"request_uri\" : \"" + req.uri() + "\" ," +
-						                " \"response\" : { \"test_array\" : [{ \"a\" : \"b\", \"c\" : \"d\" }, { \"e\" : \"f\"}]}}");
-						// String file = req.path().equals("/") ? "index.html" :
-						// req.path();
-						// req.response().sendFile("webroot/" + file);
-					}
-				}).listen(50001);
+		vertx.createHttpServer().requestHandler(rm)
+			.setSSL(true).setKeyStorePath("server-keystore.jks").setKeyStorePassword("wibble")
+			.listen(50001);
 
 		vertx.createNetServer().connectHandler(new Handler<NetSocket>() {
 			public void handle(final NetSocket socket) {
@@ -48,4 +37,137 @@ public class ServerMain {
 		}).listen(50000);
 	}
 
+	static private void initializeRouteMatcher(RouteMatcher rm) {
+
+		rm.get("/", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.put("/activation", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.put("/password", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.put("/login", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.get("/nodelist", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		// activate SA Node : /nodelist/connected/[nodeID]/[actuatorName]
+		// handle SA Node : /nodelist/connected/[nodeID]/[actuatorName]
+		rm.put("/nodelist/connected/:nodeid", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.put("/nodelist/connected/:nodeid/:actuator", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		// get current SA Node Info
+		rm.getWithRegEx("/nodelist/connected/*", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.get("/nodelist/connected", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.get("/nodelist/waiting", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		// TODO : /nodelist/black*
+		
+		// get log
+		rm.get("/log", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.get("/log/:nodeid", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		rm.get("/log/:nodeid/:saname", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				// initial connection
+				responseTemplate(req);
+			}
+		});
+
+		// Catch all - serve the index page
+		rm.getWithRegEx(".*", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				req.response().sendFile("route_match/index.html");
+			}
+		});
+	}
+
+	static private void responseTemplate(HttpServerRequest req) {
+		JsonObject resJson = new JsonObject();
+		JsonArray resParamsArr = new JsonArray();
+
+		resJson.putString("requestUri", req.uri());
+
+		for (String query : req.params().names()) {
+			JsonObject param = new JsonObject();
+
+			param.putString(query, req.params().get(query));
+			resParamsArr.addObject(param);
+		}
+		resJson.putArray("requestParams", resParamsArr);
+		System.out.println(resJson.toString());
+		System.out.println("Headers are: ");
+		for (String key : req.headers().names()) {
+			System.out.println(key + ":" + req.headers().get(key));
+		}
+		req.response().headers().add("Content-Type", "json; charset=UTF-8");
+		req.response().end(resJson.toString());
+		// .end("{ \"request_uri\" : \"" + req.uri() + "\" ," +
+		// " \"response\" : { \"test_array\" : [{ \"a\" : \"b\", \"c\" : \"d\" }, { \"e\" : \"f\"}]}}");
+		// String file = req.path().equals("/") ? "index.html" :
+		// req.path();
+		// req.response().sendFile("webroot/" + file);
+	}
 }
